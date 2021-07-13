a1969901af50d30c0983f7509e73352725cc2f3d
aditya <adityapandeyadp@gmail.com>
2021-07-13 17:55:36 +0530

    commit on main

--- voice.py
+++ voice.py
@@ -1,12 +1,15 @@
 import pyttsx3
 import speech_recognition
 
-engine = pyttsx3.init()
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices)
+engine.setProperty('voice', voices[0].id)
 
 
-def speak(some):
-    engine.say(some)
-    engine.runAndWait
+def speak(audio):  # here audio is var which contain query
+    engine.say(audio)
+    engine.runAndWait()
 
 speak("hi bro")
 
