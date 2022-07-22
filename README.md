c09e144f306258b4704a2aa99166a9a80ac1b392
aditya <adityapandeyadp@gmail.com>
2022-07-22 07:22:19 +0530

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SongRecommendationBot
  A Bot which recommend song from your search history and also create its .db file, using python
+ 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pywhatkit
 import excel2
+import time
 
 def try1():
     import excel
@@ -9,19 +10,30 @@ def try1():
 
 
 def try2():
-
     excel2.anything()
 
 
 while True:
-    var = input("type __type__or history __hist__ ")
-    if var == "type":
+    print("1.play music \n2.play music from database \n3.exit\n")
+    var = int(input("Enter your choice: "))
+    if var == 1:
         try1()
-    elif var == "hist":
-        try2()                                          
-    elif var == "HIST":
+        time.sleep(2)
+    elif var == 2:
         try2()
-    elif var == "TYPE":
-        try1()
-    else:
-        break
+        time.sleep(2)
+    elif var == 3:
+        exit()
+
+# while True:
+#     var = input("type __type__or history __hist__ ")
+#     if var == "type":
+#         try1()
+#     elif var == "hist":
+#         try2()                                          
+#     elif var == "HIST":
+#         try2()
+#     elif var == "TYPE":
+#         try1()
+#     else:
+#         break
