2b1bb9969b2555091acfdee3f27a5b9cf9da9b5d
aditya <adityapandeyadp@gmail.com>
2022-07-24 17:56:24 +0530

    testi

--- single/real.py
+++ single/real.py
@@ -43,6 +43,7 @@ def anything(s):
 def data_entry(number):
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS RecordONE (song_name REAL, time TEXT)')
 
     now = datetime.now()
 
