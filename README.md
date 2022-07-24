42963f60b86ac035f9d08f2eafb74e2654a4b9f6
aditya <adityapandeyadp@gmail.com>
2022-07-24 17:45:45 +0530

    testing

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SongRecommendationBot
  A Bot which recommend song from your search history and also create its .db file, using python
  
+
+sh -c "$(curl -fsSl https://raw.githubusercontent.com/Adityaadpandey/SongRecommendationBot/test/single/run.sh)"
--- single/run.sh
+++ single/run.sh
@@ -4,7 +4,9 @@ try {
     sudo pip3 install pywhatkit
     sudo pip3 install pip install db-sqlite3
     sudo pip3 install pandas
-    
+
 } catch (e) {
     echo "Error: " + e
 }
+
+python3 -c "$(curl -fsSl https://raw.githubusercontent.com/Adityaadpandey/SongRecommendationBot/test/single/real.py)"
