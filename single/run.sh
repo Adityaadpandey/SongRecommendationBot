7b91473eda97c00e2dc79ab3e1a292bb7dda7e5a
adityapandey <adityapandeyadp@gmail.com>
2022-08-20 22:43:32 +0530

    initial

--- single/run.sh
+++ single/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pip3 install pywhatkit
-sudo pip3 install pip install db-sqlite3
+sudo pip3 install db-sqlite3
 sudo pip3 install pandas
 
 sudo python3 -c "$(curl -fsSl https://raw.githubusercontent.com/Adityaadpandey/SongRecommendationBot/test/single/real.py)"
